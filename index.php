9957417183c01950a8dc0817a50b92d900e40bc3
deepaklogin <deepakparsabad2@gmail.com>
2020-10-11 17:16:28 +0530

    sweets
    sweets

---
+++ index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+    <title>Deepak Sweets</title>
+    <style type="text/css">
+    	@import url('https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap');
+    	*
+    	{
+    		margin: 0;
+    		padding: 0;
+    		transition: 0.4s;
+    		/*font-family: 'Amatic SC', cursive;*/
+
+    	}
+    	
+    </style>
+   <!--  <script type="text/javascript">
+	var m,i=0;
+	m=new Array('image/a1.jpg','image/a2.jpg','image/a3.jpg','image/a4.jpg','image/a5.jpg');
+	function slide1()
+	{
+		if(i<m.length-1)
+		{
+			i++;
+		}
+		else
+		{
+			i=0;
+		}
+		document.getElementById("img1").src=m[i];
+		setTimeout("slide1()",4000);
+	}
+</script> -->
+  </head>
+  <body>
+    <div class="container-fluid">
+    	<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Deepak Sweets</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#home">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#about">About</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+            Sweets types
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Jharkhandi</a></li>
+            <li><a class="dropdown-item" href="#">Kolkata</a></li>
+            <!-- <li><hr class="dropdown-divider"></li> -->
+            <li><a class="dropdown-item" href="#">Bihar</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " href="#contactus" tabindex="-1" aria-disabled="true">Contact Us</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control mr-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav><br><br><br>
+<!-- home -->
+    <section class="">
+    	<div class="row">
+    	<div class="text-center col-xl-5 col-lg-5 col-md-12 col-sm-8 col-xs-11 container mx-auto" id="home">
+    		<div id="carouselExampleFade" class="carousel slide carousel-fade col-md-6 col-xs-6 mx-auto" data-ride="carousel" style="width: 370px;">
+    			<div class="carousel-inner text-center" style="border-radius: 50px;">
+    				<div class="carousel-item active">
+    					<img src="image/a1.jpg" class="d-block w-100" alt="Sweets Images">
+    				</div>
+    				<div class="carousel-item">
+    					<img src="image/a2.jpg" class="d-block w-100" alt="Sweets Images">
+    				</div>
+    				<div class="carousel-item">
+    					<img src="image/a3.jpg" class="d-block w-100" alt="Sweets Images">
+    				</div>
+    			</div>
+    			<a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+    				<span class="carousel-control-prev-icon " aria-hidden="true"></span>
+    				<span class="sr-only">Previous</span>
+    			</a>
+    			<a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+    				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+    				<span class="sr-only">Next</span>
+    			</a>
+    		</div>	
+        </div><br><br>
+        <div class="col-xl-7 col-lg-6 col-md-12 col-sm-10 col-xs-10 mx-auto mt-4" id="about" >
+        	<div class="container">
+        		<h1 class="text-primary ">About Sweets</h1>
+        		<hr class="divider">
+        		<p style="text-align: justify;">Sweets from the Indian subcontinent are the confectionery and desserts of the Indian subcontinent.[1][2] Thousands of dedicated shops in India, Pakistan, Bangladesh, Nepal and Sri Lanka sell nothing but sweets.
+
+        		Sugarcane has been grown in the Indian subcontinent for thousands of years, and the art of refining sugar was invented there 8000 years ago (6000 BCE) by the Indus Valley Civilisation.[3][4][5] The English word "sugar" comes from a Sanskrit word sharkara for the refined sugar, while the word "candy" comes from Sanskrit word khaanda for the unrefined sugar– one of the simplest raw forms of sweet.[6] Over its long history, cuisines of the Indian subcontinent developed a diverse array of sweets. Some[5] claim there is no other region in the world where sweets are so varied, so numerous, or so invested with meaning as the Indian subcontinent.[7]In the diverse languages of the Indian subcontinent, sweets are called by numerous names, one common name being Mithai (मिठाई, মিঠাই, مٹھائی). They include sugar, and a vast array of ingredients such as different flours, milk, milk solids, fermented foods, root vegetables, raw and roasted seeds, seasonal fruits, fruit pastes and dry fruits.[8] Some sweets such as kheer are cooked, some like burfi are baked, varieties like Mysore pak are roasted, some like jalebi are fried, others like kulfi are frozen, while still others involve a creative combination of preparation techniques.[9][10][11] The composition and recipes of the sweets and other ingredients vary by region. Mithai are sometimes served with a meal, and often included as a form of greeting, celebration, religious offering, gift giving, parties, and hospitality in the Indian subcontinent. On South Asian festivals – such as Holi, Diwali, Eid, and Raksha Bandhan – sweets are homemade or purchased, then shared.[2][12] Many social gatherings, wedding ceremonies and religious festivals often include a social celebration of food, and the flavors of sweets are an essential element of such a celebration.[13]</p>
+        	</div>
+        </div>
+    </div>
+    </section><br><br>
+    <hr class="divider">
+    </div>
+    <section id="contactus" class="container">
+    	<div class="col-xl-5 col-lg-7 col-md-8 col-sm-12 col-xs-11 mx-auto">
+    		<h2 class="text-success text-center">Contact Us</h2>
+    		<hr class="divider">
+    		<form class="form">
+    			<div class="input-group mb-4">
+    				<span class="input-group-text">Name</span>
+    				<input type="text" name="" class="form-control">
+    			</div>
+    			<div class="input-group mb-4">
+    				<span class="input-group-text">Email</span>
+    				<input type="text" name="" class="form-control">
+    				<span class="input-group-text">@gmail.com</span>
+    			</div>
+    			<div class="input-group mb-4">
+    				<span class="input-group-text">Your Favourite Sweets</span>
+    				<select class="form-select">
+    					<option>Select Sweets</option>
+    					<option>Shrikhand</option>
+    					<option>Gajar Ka Halwa</option>
+    					<option>Rasmalai</option>
+    					<option>Ghevar</option>
+    				</select>
+    			</div>
+    			<div class="input-group mb-4">
+    				<span class="input-group-text">Phone Number</span>
+    				<input type="text" name="" class="form-control">
+    			</div>
+    			<div class="form-file mb-4">
+    				<input type="file" class="form-file-input" name="">
+    				<label class="form-file-label">
+    					<span class="form-file-text">Sweets Photo</span>
+    					<span class="form-file-button">Browse File</span>
+    				</label>
+    			</div>
+    			<div class="form-check form-switch mb-4">
+    				<input class="form-check-input" type="checkbox" name="">
+    				<label class="form-check-label">please check</label>
+    			</div>
+    			<div class="form-check">
+    				<input type="checkbox" name="" class="form-check-input ">
+    				<span class="form-check-label text-decoration-none" href="#">Term & Condition are apply...<a href="">Click Here</a></span>
+    			</div>
+    			<div class="form-group text-center mt-4">
+    				<button class="btn btn-outline-primary px-4">Submit</button>
+    			</div>
+    		</form>
+    	</div>
+    </section><br><br><br>
+    <!-- Optional JavaScript -->
+    <!-- Popper.js first, then Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
+  </body>
+</html>
